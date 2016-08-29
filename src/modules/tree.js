fe531354a7e5d4b2fa41858c08bf4ce6f7f28d95
Jake Bian <jake@keplr.io>
2016-08-28 18:50:08 -0700

    dev: setup project and basic reducer

---
+++ src/modules/tree.js
@@ -0,0 +1,18 @@
+const updateActionType = 'tree-update'
+
+export function updateTree (tree) {
+    return {
+        type: updateActionType,
+        tree: tree
+    };
+}
+
+
+export function treeReducer (state = { isEmpty: true }, action) {
+    switch (action.type) {
+        case updateActionType:
+            return action.tree || state;
+    }
+
+    return state;
+}
