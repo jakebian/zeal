315d4ef9df1f9d7e8e2250a26aba20bd67283a53
Jake Bian <jake@keplr.io>
2016-08-28 19:11:56 -0700

    feature: basic views and tree computation

---
+++ src/components/TreeVis/tree-vis.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import cytoscape from 'cytoscape';
+import cydagre from 'cytoscape-dagre';
+import dagre from 'dagre';
+import { computeGraph } from 'components/TreeVis/util';
+
+export class TreeVis extends Component {
+    componentDidMount() {
+        let cyGraph = cytoscape({
+            container: this.refs.treeContainer,
+            elements: computeGraph(this.props.tree),
+            layout: {
+              name: 'dagre',
+              rankDir: 'LR'
+            },
+            style: [
+                {
+                    selector: 'node',
+                    style: {
+                        'content': 'data(id)',
+                        'text-opacity': 0.5,
+                        'text-valign': 'bottom',
+                        'text-halign': 'center',
+                        'text-margin-y': 15,
+                    }
+                },
+                {
+                    selector: 'edge',
+                    style: {
+                        'width': 4,
+                        'target-arrow-shape': 'triangle',
+                        'curve-style': 'bezier'
+                    }
+                }
+            ]
+        });
+
+    }
+    render() {
+        <div>
+            <div ref='treeContainer' className='tree-container'></div>
+        </div>
+    }
+}
+
+export default connect((state) => {
+    tree: state.tree
+}, {})(TreeVis)
---
+++ src/components/TreeVis/util.js
@@ -0,0 +1,35 @@
+function computeGraph(tree) {
+    return getGraphNodes(tree).concat(getGraphLinks(tree))
+}
+
+function getGraphNodes(tree) {
+    return [{
+        data: {
+            id: tree.id,
+            data: tree.data
+        }
+    }].concat(
+        tree.children ? tree.children.reduce(
+            (nodes, child) => nodes.concat(getGraphNodes(child)), []
+        ) :[]
+    )
+}
+
+function getGraphLinks(tree) {
+
+    return tree.children ?
+
+        tree.children.reduce((links, child) => (
+            links.concat(
+                [{
+                    data: {
+                        id: tree.id + '-' + child.id,
+                        source: tree.id,
+                        target: child.id
+                    }
+                }].concat(getGraphLinks(child))
+            )
+        ), [])
+
+        : []
+}
